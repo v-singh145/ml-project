86050e2872a6bf0320e6e802a5d0dd770a9a0fb4
Ayush <ayushupadhyay0606@gmail.com>
2025-12-25 13:18:56 +0530

    Initial commit: SMS Spam Classifier with Streamlit

---
+++ app.py
@@ -0,0 +1,56 @@
+import streamlit as st
+import pandas as pd
+import numpy as np
+import pickle
+import nltk
+from nltk.corpus import stopwords
+import string
+from nltk.stem.porter import PorterStemmer
+
+# Download required NLTK data
+nltk.download('punkt_tab')
+nltk.download('stopwords')
+
+ps = PorterStemmer()
+
+def transform_text(text):
+  text = text.lower()
+  text = nltk.word_tokenize(text)
+
+  y=[]
+
+  for i in text:
+    if i.isalnum():
+      y.append(i)
+
+  text = y[:]
+  y.clear()
+
+  for i in text:
+    if i not in stopwords.words('english') and i not in string.punctuation:
+      y.append(i)
+
+  text = y[:]
+  y.clear()
+
+  for i in text:
+    y.append(ps.stem(i))
+
+  return " ".join(y)
+
+tfidf = pickle.load(open('vectorizer.pkl', 'rb'))
+model = pickle.load(open('model.pkl', 'rb'))
+st.title("Email/SMS Spam Classifier")
+input_sms = st.text_area("Enter the message")
+
+if st.button("Check Spam"):
+  if input_sms.strip():
+    transform_sms = transform_text(input_sms)
+    vector_input = tfidf.transform([transform_sms])
+    result = model.predict(vector_input)[0]
+    if result == 1:
+      st.header("🚨 Spam")
+    else:
+      st.header("✅ Not Spam")
+  else:
+    st.warning("Please enter a message first!")
